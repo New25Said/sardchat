eff1abc169319f0371a9457c37af36ef38c89ff0
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-04-11 18:25:10 -0500

    Refactor user registration and login functions

--- public/app.js
+++ public/app.js
@@ -2,30 +2,37 @@ const socket = io();
 
 let username = "";
 
-function entrar() {
-  username = document.getElementById("username").value;
-
-  if (!username) return alert("Pon username");
-
-  socket.emit("nuevo_usuario", username);
-
-  document.getElementById("login").style.display = "none";
-  document.getElementById("chat").style.display = "block";
+function registro() {
+  socket.emit("registro", {
+    username: user.value,
+    password: pass.value
+  });
 }
 
+function login() {
+  socket.emit("login", {
+    username: user.value,
+    password: pass.value
+  });
+}
+
+socket.on("login_ok", (user) => {
+  username = user;
+  login.style.display = "none";
+  chat.style.display = "block";
+});
+
+socket.on("error_login", (msg) => {
+  alert(msg);
+});
+
 function enviar() {
-  const input = document.getElementById("msg");
-
-  if (!input.value) return;
-
-  socket.emit("mensaje", input.value);
-  input.value = "";
+  socket.emit("mensaje", msg.value);
+  msg.value = "";
 }
 
 socket.on("mensaje", (data) => {
   const div = document.createElement("div");
-  div.className = "mensaje";
   div.textContent = data.user + ": " + data.text;
-
-  document.getElementById("mensajes").appendChild(div);
+  mensajes.appendChild(div);
 });
