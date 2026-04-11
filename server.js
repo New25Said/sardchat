00e5ebc550e6a8b1e390d85f41b85df64ff2f3d0
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-04-11 15:01:24 -0500

    Initialize Express server with Socket.IO integration

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+let usuarios = {};
+
+io.on("connection", (socket) => {
+  console.log("Conectado");
+
+  socket.on("nuevo_usuario", (username) => {
+    usuarios[socket.id] = username;
+  });
+
+  socket.on("mensaje", (msg) => {
+    io.emit("mensaje", {
+      user: usuarios[socket.id],
+      text: msg
+    });
+  });
+
+  socket.on("disconnect", () => {
+    delete usuarios[socket.id];
+  });
+});
+
+// 🔥 IMPORTANTE para Render
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Servidor corriendo en " + PORT);
+});
