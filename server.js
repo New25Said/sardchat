88d4b1f496a66748b74171a5f50f98b08f1a1a5a
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-04-11 18:24:18 -0500

    Implement user registration and login functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,13 +10,45 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 let usuarios = {};
+let cuentas = JSON.parse(fs.readFileSync("users.json"));
 
+// Registrar usuario
 io.on("connection", (socket) => {
 
-  socket.on("nuevo_usuario", (username) => {
-    usuarios[socket.id] = username;
+  socket.on("registro", ({ username, password }) => {
+    if (cuentas[username]) {
+      socket.emit("error_login", "Usuario ya existe");
+      return;
+    }
+
+    if (!/^[a-z0-9]+$/.test(username)) {
+      socket.emit("error_login", "Username inválido");
+      return;
+    }
+
+    cuentas[username] = { password };
+    fs.writeFileSync("users.json", JSON.stringify(cuentas));
+
+    socket.emit("login_ok", username);
   });
 
+  // Login
+  socket.on("login", ({ username, password }) => {
+    if (!cuentas[username]) {
+      socket.emit("error_login", "No existe");
+      return;
+    }
+
+    if (cuentas[username].password !== password) {
+      socket.emit("error_login", "Contraseña incorrecta");
+      return;
+    }
+
+    usuarios[socket.id] = username;
+    socket.emit("login_ok", username);
+  });
+
+  // Mensajes
   socket.on("mensaje", (msg) => {
     io.emit("mensaje", {
       user: usuarios[socket.id],
