13eaa7f25c0ec618de1ccf5c10e850c425127d75
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-04-17 21:26:36 -0500

    Update comments and server log message

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ let chatHistory = [];
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-    // Al entrar, enviamos todo lo que existe en la RAM
+    // Enviar historial al entrar
     socket.emit('load history', chatHistory);
 
     socket.on('message', (data) => {
-        // Guardamos el mensaje exactamente como llegó
+        // Añadir ID de socket para identificar mensajes propios
+        data.socketId = socket.id;
         chatHistory.push(data);
-        // Lo enviamos a todos los conectados
         io.emit('new message', data);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Servidor WineChat Online'));
+server.listen(PORT, () => console.log('WineChat Ultra Arriba'));
