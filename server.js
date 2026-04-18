38beb9314bf387385c58865b8aab8b98d04a0889
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-04-17 20:35:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,23 +12,13 @@ let activeUsers = {};
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 io.on('connection', (socket) => {
-    // Al conectar enviamos el historial y los usuarios actuales
     socket.emit('init', { history: chatHistory, users: activeUsers });
 
     socket.on('join', (data) => {
-        activeUsers[socket.id] = { 
-            nickname: data.nickname, 
-            photo: data.photo 
-        };
-        
+        activeUsers[socket.id] = { nickname: data.nickname, photo: data.photo };
         const systemMsg = { text: `${data.nickname} se ha unido`, type: 'system' };
         chatHistory.push(systemMsg);
-        
         io.emit('user list update', activeUsers);
         io.emit('message', systemMsg);
     });
@@ -37,23 +27,35 @@ io.on('connection', (socket) => {
         if (activeUsers[socket.id]) {
             const oldName = activeUsers[socket.id].nickname;
             activeUsers[socket.id] = { nickname: data.nickname, photo: data.photo };
-            
             io.emit('user list update', activeUsers);
             io.emit('message', { text: `${oldName} ahora es ${data.nickname}`, type: 'system' });
         }
     });
 
-    socket.on('message', (text) => {
-        if (activeUsers[socket.id]) {
-            const msgData = {
-                text: text,
-                userId: socket.id,
-                type: 'user',
-                time: Date.now()
-            };
-            chatHistory.push(msgData);
-            io.emit('message', msgData);
+    socket.on('message', (data) => {
+        if (!activeUsers[socket.id]) return;
+        const user = activeUsers[socket.id];
+        let msgData = {
+            userId: socket.id,
+            userNick: user.nickname, // Guardamos copia para que no se pierda al salir
+            userPhoto: user.photo,
+            time: Date.now()
+        };
+
+        // Comandos
+        if (data.startsWith('/sys ')) {
+            msgData.text = data.replace('/sys ', '');
+            msgData.type = 'system';
+        } else if (data.startsWith('/aviso ')) {
+            msgData.text = data.replace('/aviso ', '');
+            msgData.type = 'aviso';
+        } else {
+            msgData.text = data;
+            msgData.type = 'user';
         }
+
+        chatHistory.push(msgData);
+        io.emit('message', msgData);
     });
 
     socket.on('disconnect', () => {
@@ -61,10 +63,10 @@ io.on('connection', (socket) => {
             const name = activeUsers[socket.id].nickname;
             delete activeUsers[socket.id];
             io.emit('user list update', activeUsers);
-            io.emit('message', { text: `${name} ha salido del chat`, type: 'system' });
+            io.emit('message', { text: `${name} ha salido`, type: 'system' });
         }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Servidor WineChat escuchando en puerto ' + PORT));
+server.listen(PORT, () => console.log('WineChat Pro en puerto ' + PORT));
