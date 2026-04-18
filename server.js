61f5fada88a09582fe909898901c655d8f05679a
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-04-17 22:43:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 let chatHistory = [];
 app.use(express.static(__dirname));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 io.on('connection', (socket) => {
+    // Enviar historial al entrar
     socket.emit('load-history', chatHistory);
+
+    // Recibir mensaje y repartirlo
     socket.on('send-message', (data) => {
         data.socketId = socket.id;
         chatHistory.push(data);
@@ -19,4 +27,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('OK'));
+server.listen(PORT, () => console.log('Servidor WineChat Online en puerto ' + PORT));
